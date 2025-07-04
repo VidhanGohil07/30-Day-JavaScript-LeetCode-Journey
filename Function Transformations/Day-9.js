a2c0daeb9ecbcd38bd8f60b2f6e320c91963c727
Vidhan Gohil <vidhangohil.imscit20@gmail.com>
2025-07-04 17:58:05 +0000

    Updated Day-9 JavaScript Journey

---
+++ Function Transformations/Day-9.js
@@ -0,0 +1,18 @@
+// Return Length of Arguments Passed
+// Write a function argumentsLength that returns the count of arguments passed to it.
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    // return args.length
+    let count = 0
+    for(let i =0; i <args.length ; i++){
+        count += 1
+    }
+    return count
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
